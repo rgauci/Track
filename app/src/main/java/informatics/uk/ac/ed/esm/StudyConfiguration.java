05955dd63345d48f14bf67d0d5ea5e6aee7a85f8
rachelgauci <gauci.rachel@gmail.com>
2015-06-12 17:51:52 +0100

    Finished Study Configuration styling.

--- app/src/main/java/informatics/uk/ac/ed/esm/StudyConfiguration.java
+++ app/src/main/java/informatics/uk/ac/ed/esm/StudyConfiguration.java
@@ -4,20 +4,36 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class StudyConfiguration extends ActionBarActivity {
 
+    private TextView txtStartDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_study_configuration);
+        setContentView(R.layout.activity_study_configuration2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_study_configuration, menu);
+
+        txtStartDate = (TextView) findViewById(R.id.txtStartDate);
+
+        /* set start date to the following day */
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+        SimpleDateFormat sdf = new SimpleDateFormat(getResources().getString(R.string.defaultDateFormat));
+        txtStartDate.setText(sdf.format(calendar.getTime()));
+
         return true;
     }
 
