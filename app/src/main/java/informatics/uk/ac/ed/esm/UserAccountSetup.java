306f82e31b65bdeefd6baa96da59a688e00e3c50
rachelgauci <gauci.rachel@gmail.com>
2015-06-12 18:25:50 +0100

    User Account setup interface.

--- app/src/main/java/informatics/uk/ac/ed/esm/UserAccountSetup.java
+++ app/src/main/java/informatics/uk/ac/ed/esm/UserAccountSetup.java
@@ -1,39 +1,23 @@
 package informatics.uk.ac.ed.esm;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 
-public class StudyConfiguration extends ActionBarActivity {
-
-    private TextView txtStartDate;
+public class UserAccountSetup extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_study_configuration2);
-
-        /* set start date to the following day */
-        txtStartDate = (TextView) findViewById(R.id.txtStartDate);
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, 1);
-        SimpleDateFormat sdf = new SimpleDateFormat(getResources().getString(R.string.defaultDateFormat));
-        txtStartDate.setText(sdf.format(calendar.getTime()));
+        setContentView(R.layout.activity_user_account_setup);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_study_configuration, menu);
+        getMenuInflater().inflate(R.menu.menu_user_account_setup, menu);
         return true;
     }
 
@@ -50,10 +34,5 @@ public class StudyConfiguration extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void btnNext_onClick(View view) {
-        Intent intent = new Intent(this, UserAccountSetup.class);
-        startActivity(intent);
     }
 }
