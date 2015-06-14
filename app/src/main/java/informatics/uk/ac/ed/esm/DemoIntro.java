c760ca5bb413bc572f9795a025a35493c72d215d
rachelgauci <gauci.rachel@gmail.com>
2015-06-14 14:01:48 +0100

    Added Demo Intro screen.

--- app/src/main/java/informatics/uk/ac/ed/esm/DemoIntro.java
+++ app/src/main/java/informatics/uk/ac/ed/esm/DemoIntro.java
@@ -1,42 +1,24 @@
 package informatics.uk.ac.ed.esm;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class SetupComplete extends AppCompatActivity {
-
-    private Button btnDemoNo;
-    private Button btnDemoYes;
+public class DemoIntro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup_complete);
-
-        btnDemoNo = (Button) findViewById(R.id.btnDemoNo);
-        btnDemoYes = (Button) findViewById(R.id.btnDemoYes);
-
-        btnDemoYes.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent intent = new Intent(SetupComplete.this, DemoIntro.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_demo_intro);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setup_complete, menu);
+        getMenuInflater().inflate(R.menu.menu_demo_intro, menu);
         return true;
     }
 
