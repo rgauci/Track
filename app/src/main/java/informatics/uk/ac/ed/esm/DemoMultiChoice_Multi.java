a6e8019a8d96f09ab828a168f3e6231dd79ca48c
rachelgauci <gauci.rachel@gmail.com>
2015-06-21 16:06:00 +0100

    Added validation to Demo Multi-Choice Multi-Answer.

--- app/src/main/java/informatics/uk/ac/ed/esm/DemoMultiChoice_Multi.java
+++ app/src/main/java/informatics/uk/ac/ed/esm/DemoMultiChoice_Multi.java
@@ -8,27 +8,45 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class DemoMultiChoice_Multi extends AppCompatActivity {
 
-    private Button btnNext;
-    private Button btnPrevious;
+    private Button btnNext, btnPrevious;
+    private ArrayList<CheckBox> checkBoxes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_multi_choice__multi);
 
-        /* set onClick listeners */
+        // initialise UI controls
         btnNext = (Button) findViewById(R.id.btnNext);
         btnPrevious = (Button) findViewById(R.id.btnPrevious);
 
+        checkBoxes = new ArrayList<CheckBox>();
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx1));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx2));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx3));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx4));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx5));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx6));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx7));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx8));
+        checkBoxes.add((CheckBox) findViewById(R.id.chkBx9));
+
+        // set onClick listeners
         btnNext.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DemoMultiChoice_Multi.this, DemoScale.class);
-                startActivity(intent);
+                if (isValid()) {
+                    Intent intent = new Intent(DemoMultiChoice_Multi.this, DemoScale.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -60,5 +78,28 @@ public class DemoMultiChoice_Multi extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isValid() {
+        boolean hasErrors = false;
+
+        boolean atLeastOneSelected = false;
+        int checked = 0;
+
+        while ((!atLeastOneSelected) && (checked < this.checkBoxes.size())) {
+            if (this.checkBoxes.get(checked).isChecked()) {
+                atLeastOneSelected = true;
+            }
+            checked++;
+        }
+
+        if (!atLeastOneSelected) {
+            Toast toast = Toast.makeText(this,
+                    getResources().getString(R.string.error_answerToProceed), Toast.LENGTH_SHORT);
+            toast.show();
+            hasErrors = true;
+        }
+
+        return !hasErrors;
     }
 }
